cddf4a0840ce1ccec76a20c9bafc1b62c070fa99
Mayan <gabrielmayan@gmail.com>
2022-05-01 18:35:56 -0300

    Correção de bugs

--- backend/src/middlewares/validateRequest.js
+++ backend/src/middlewares/validateRequest.js
@@ -1,12 +1,3 @@
-const validateRequest = (schema) => async (request, response, next) => {
-	try {
-		await schema.validate(request);
-		next();
-	} catch (error) {
-		return response.status(500).json(error.message);
-	}
-};
-
 const validateBody = (schema) => async (request, response, next) => {
 	try {
 		await schema.validate(request.body);
@@ -16,31 +7,4 @@ const validateBody = (schema) => async (request, response, next) => {
 	}
 };
 
-const validateParams = (schema) => async (request, response, next) => {
-	try {
-		await schema.validate(request.params);
-		next();
-	} catch (error) {
-		return response.status(500).json(error.message);
-	}
-};
-
-const validateQuery = (schema) => async (request, response, next) => {
-	try {
-		await schema.validate(request.query);
-		next();
-	} catch (error) {
-		return response.status(500).json(error.message);
-	}
-};
-
-const validateUser = (schema) => async (request, response, next) => {
-	try {
-		await schema.validate(request.user);
-		next();
-	} catch (error) {
-		return response.status(500).json(error.message);
-	}
-};
-
-module.exports = { validateRequest, validateBody, validateParams, validateQuery, validateUser };
+module.exports = { validateBody };
