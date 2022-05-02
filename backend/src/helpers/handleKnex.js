62b19833a323588573aafce09d4d5abd6ba1d5d8
JoaoOliveira0117 <joao.oliveira0117@hotmail.com>
2022-05-01 22:35:31 -0300

    updates for only frontend being the project here

--- backend/src/helpers/handleKnex.js
+++
@@ -1,8 +0,0 @@
-const knex = require('../services/knex');
-
-const getInfo = async (table) => await knex(table);
-const getInfoWithConditions = async (table, conditons) => await knex(table).where(conditons);
-
-const insertInfo = async (table, values) => await knex(table).insert(values);
-
-module.exports = { insertInfo, getInfo, getInfoWithConditions }
--- backend/src/helpers/handleUuid.js
+++
@@ -1,5 +0,0 @@
-const uuid = require('uuid');
-
-const generateUuid = (info) => uuid.v4(info);
-
-module.exports = { generateUuid };
